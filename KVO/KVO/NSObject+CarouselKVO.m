3ada0b4a48516fddf9b9bb5153e15ca9ecfa0275
Chenxiaolong <chenxiaolong@youku.com>
2016-10-18 22:28:54 +0800

    自定义实现KVO部分代码

--- KVO/KVO/NSObject+CarouselKVO.m
+++ KVO/KVO/NSObject+CarouselKVO.m
@@ -10,7 +10,7 @@
 
 #import <objc/runtime.h>
 
-
+static const NSString* CarouselKVOClassPrefix = @"CarouselKVOClassPrefix";
 
 @implementation NSObject (CarouselKVO)
 
@@ -18,6 +18,56 @@
 
 - (void)addCarouselObserver:(id)observer keyPath:(NSString* )keyPath withNotifyBlock:(NotifyBlock)block;
 {
+    //1.首先检查传入的key有没有实现set方法 
+    NSString* setter = [NSString stringWithFormat:@"set%@:",[keyPath capitalizedString]];
+    SEL selector = NSSelectorFromString(setter);
+    //查看有没有实现set方法
+    Method method  = class_getInstanceMethod([self class], selector);
+    if (!method)
+    {
+        NSException* customException = [NSException exceptionWithName:NSInvalidArgumentException reason:[NSString stringWithFormat:@"%@未实现set方法",keyPath] userInfo:nil ];
+        @throw customException;//这里抛出异常 可以在控制台查看具体错误信息
+    }
+    //2.生成中间类
+    
+}
+
+-(Class)intermediateClassFromOriginaClass:(NSString* ) classString
+{
+    //获取被观察对象的类 用于中间类指向它
+    Class originalClass = NSClassFromString(classString);
+    
+    //依照系统命名的规则拼接出中间类名
+    NSString* intermediateClassName = [NSString stringWithFormat:@"%@%@",CarouselKVOClassPrefix,classString];
+    
+    /*
+    利用runtime方法动态生成中间类
+     
+    Class objc_allocateClassPair ( Class superclass, const char *name, size_t extraBytes );
+    
+     参数解释
+    return        返回新创建的类
+    superclass    新建类的父类，如果传nil则创建新的根类
+    name          新建类的名称
+    extraBytes    The number of bytes to allocate for indexed ivars at the end of the class and 
+                  metaclass objects. This should usually be 0 具体作用不清楚 以后查清楚补上解释 字面意思
+                  为为类和元类结尾的需要索引化的变量提供空间？总之文档就让传0
+     */
+   Class intermediateClass = objc_allocateClassPair(originalClass, intermediateClassName.UTF8String, 0);
+    
+   //重写中间类的class方法 用于添加观察者之后 被观察对象调用class方法可以和未添加观察者之前返回同一个类型-，- 不敢确定有什么卵用 猜测系统之所以这么做应该是为了第一不想过度暴露KVO实现的细节  第二应该是防止调用class方法返回的对象在使用KVO之后不一致会造成一些问题 比如说判断是否属于某个类之类的 总之是为了隐藏KVO所带来的不必要变化
+    [self rewriteClassMethod];
+    
+    
+    
+    return nil;
+}
+
+
+-(void)rewriteClassMethod
+{
+    //获取默认的class方法
+    Method classMethod = class_getClassMethod([self class], @selector(class));
     
 }
 
--- KVO/KVO/ViewController.m
+++ KVO/KVO/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "Observer.h"
 #import "Person.h"
-
+#import "NSObject+CarouselKVO.h"
 
 
 @interface ViewController ()
@@ -48,7 +48,7 @@
      3.如果被观察者实现了automaticallyNotifiesObserversForKey:(NSString* ) key 方法并且对指定的key返回了NO则不会自动触发KVO 也就是通过set方法和kvc赋值的方法都不会触发kvo 但是手动调用了willchange 和didchange方法的就可以
      */
     
-    
+    [_person removeObserver:_obs forKeyPath:@"name"];
     
     [self somethingInteresting];
     
@@ -56,11 +56,49 @@
 
 
 
+
+
+
+
+
+
 /**
- 自定义实现KVO 通过为根类写一个category的方式以便所有子类都可以调用
+ 自定义实现KVO 通过为根类NSObject写一个category的方式以便所有子类都可以调用
+ 
+ 
+ 简述KVO的实现：
+    当你观察一个对象时，一个新的类会动态被创建。这个类继承自该对象的原本的类，并重写了被观察属性的 setter 方法。自然，重写的 setter 方法会负责在调用原 setter 方法之前和之后，通知所有观察对象值的更改。最后把这个对象的 isa 指针 ( isa 指针告诉 Runtime 系统这个对象的类是什么 ) 指向这个新创建的子类，对象就神奇的变成了新创建的子类的实例。
+    原来，这个中间类，继承自原本的那个类。不仅如此，Apple 还重写了 -class 方法，企图欺骗我们这个类没有变，就是原本那个类。
+    下面的Log可以看到具体的变化：
+ 
+ 
  */
 -(void)somethingInteresting
 {
+    self.person = [[Person alloc] init];
+    NSLog(@"初始化状态：Class : %@    isa :%@",[_person class],[_person valueForKey:@"isa"]);
+    self.obs = [[Observer alloc] init];
+    [_person addObserver:_obs forKeyPath:@"name" options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:NULL];
+    NSLog(@"添加观察者之后状态：Class : %@   isa : %@",[_person class],[_person valueForKey:@"isa"]);
+    Class currentClass = [[_person valueForKey:@"isa"] superclass];
+    NSLog(@"添加观察者之后状态：isa superClass : %@",currentClass);
+
+    /**
+     2016-10-18 17:48:41.817 初始化状态:        Class : Person    isa :Person
+     2016-10-18 17:48:41.818 添加观察者之后状态： Class : Person    isa :NSKVONotifying_Person
+     2016-10-18 18:50:48.318 添加观察者之后状态： isa superClass : Person
+
+     可以看到和上面所说的一样会生成一个中间类 NSKVONotifying_Person -->Person   - -但是很明显Apple重写了中间类的class方法  所以返回的class仍然是Person
+     然后会重写其set方法 在修改值的方法前后 添加willchange 和 didchange方法 用来通知观察者 被观察对象的属性的值的变化 具体可以看下36-44行处的代码
+    */
+    
+    Person* carousel = [[Person alloc] init];
+    [carousel addCarouselObserver:self keyPath:@"name" withNotifyBlock:^(NSString *keyPath, id obj, NSDictionary *change) {
+        
+    }];
+    
+    
+    
     
 }
 
